08ecacd82fbf2a0017baad64c5d0029235e77514
Sultanbek <sultan.02173@gmail.com>
2023-02-22 19:09:42 +0600

    ispravil DZ1

--- core/settings.py
+++ core/settings.py
@@ -16,13 +16,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent 
 
-load_dotenv(os.path.join(BASE_DIR, ".evn"))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+    
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!

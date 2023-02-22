1f14abb9fbb3667f3da2ad26acc53f3462080807
Sultanbek <sultan.02173@gmail.com>
2023-02-22 20:49:28 +0600

    Hw1

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class HelloTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_hello(self):
+        responce = self.client.get(reverse("hello-view"))
+        expected_data = "<h1>Hello</h1>"
+        expected_header = "Alex"
+
+        self.assertEqual(responce.content.decode(), expected_data)
+        self.assertEqual(responce.status_code, 500)
+        self.assertEqual(responce.headers["name"], expected_header)
+
+
+    def test_get_index(self):
+        responce_get = self.client.get(reverse("index-page"))
+        responce_post = self.client.post(reverse("index-page"))
+
+        expect_get = "Главная страница"
+        expect_post = "Не тот запрос"
+
+        self.assertEqual(responce_get.status_code, 200)
+        self.assertEqual(responce_post.status_code, 200)
+        self.assertEqual(responce_get.content.decode(), expect_get)
+        self.assertEqual(responce_post.content.decode(), expect_post)
+
+
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def hello(request):
+    # my_list = [1, 2, 3, 4]
+    body = "<h1>Hello</h1>"
+    # body = """
+    #     <!DOCTYPE html>
+    #         <html>
+    #             <head>
+    #                 <title>GeekTest</title>
+    #             </head>
+    #             <body>
+
+    #                 <h1>ЗАголовок 1 уровня</h1>
+    #                 <p>Параграф</p>
+
+    #             </body>
+    #         </html>
+    # """
+    headers = {"name":"Alex",
+                "Content-type":"application/vnd.ms-excel",
+                "Content-Disposition":"attachment; filename = file.xls"
+                }
+    return HttpResponse(body, headers = headers, status = 500)
+
+
+def get_index(request):
+    print(request.user)
+    if request.method == "GET":
+        return HttpResponse("Главная страница")
+    else:
+        return HttpResponse("Не тот запрос")
+
 
 # Create your views here.

7c1e2b5a757c3628225b3e04bb4eb8192ad10a57
Sultanbek <sultan.02173@gmail.com>
2023-03-11 18:10:29 +0600

    Ispravlenie v indexView

--- posts/views.py
+++ posts/views.py
@@ -41,14 +41,14 @@ def hello(request):
 #     return render(request, "posts/index.html", context=context)
 
 class IndexView(generic.ListView):
-    # queryset = Post.objects.filter(status=True)
+    queryset = Post.objects.filter(status=True)
     context_object_name = "posts"
-    model = Post
+    # model = Post
     template_name = "posts/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["form"] = PostForm()
+        context["title"] = "Просмотр поста"
         return context
 
 
